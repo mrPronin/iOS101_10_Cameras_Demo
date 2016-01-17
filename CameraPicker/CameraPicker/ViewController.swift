b8904ab82e1dadd96ec80e8c2be64b008317b8a5
Alexander Pronin <pronin.alx@gmail.com>
2016-01-17 15:53:43 +0200

    minor changes

---
+++ CameraPicker/CameraPicker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CameraPicker
+//
+//  Created by Brian on 9/18/15.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  @IBAction func pick(sender: AnyObject) {
+  }
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
